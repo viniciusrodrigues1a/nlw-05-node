9022e2434370de3f69db25fc1b92610c32b12ff5
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-20 16:48:45 -0300

    aula 02 - maximum speed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import "./database";
+import {routes} from "./routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
