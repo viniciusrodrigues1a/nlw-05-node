304d85d49525997a5f4162812dd725d60e2def49
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-21 15:11:46 -0300

    aula 03 - in orbit

---
+++ src/services/MessagesService.ts
@@ -0,0 +1,40 @@
+import {getCustomRepository, Repository} from "typeorm";
+import {Message} from "../entities/Message";
+import {MessagesRepository} from "../repositories/MessagesRepository";
+
+interface IMessagesCreate {
+  admin_id?: string;
+  user_id: string;
+  text: string;
+}
+
+class MessagesService {
+  private messagesRepository: Repository<Message>;
+
+  constructor() {
+    this.messagesRepository = getCustomRepository(MessagesRepository);
+  }
+
+  async create({ admin_id, user_id, text }: IMessagesCreate) {
+    const message = this.messagesRepository.create({
+      admin_id,
+      user_id,
+      text
+    });
+
+    await this.messagesRepository.save(message);
+
+    return message;
+  }
+
+  async listByUser(user_id: string) {
+    const messages = await this.messagesRepository.find({
+      where: { user_id },
+      relations: ["user"]
+    });
+
+    return messages;
+  }
+}
+
+export {MessagesService};
