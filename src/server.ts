a7658a514444a73ce02d6134c6a8838fe30b7044
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-04-23 15:23:59 -0300

    aula 05 - surface exploration

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { http } from "./http";
 import "./websocket/client";
+import "./websocket/admin"
 
 const port = 3333;
 http.listen(port, () => console.log(`Server is running on port ${port}`));
